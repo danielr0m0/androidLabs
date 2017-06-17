56007cd63375bff8686a3a185451b7d48cf26948
danielr0m0 <drdanielromo@gmail.com>
2017-06-16 22:56:35 -0700

    add homework folder with text dir and newsApp

---
+++ homework/NewsApp/app/src/main/java/com/romodaniel/newsapp/NetworkUtils.java
@@ -0,0 +1,13 @@
+package com.romodaniel.newsapp;
+
+/**
+ * Created by drdan on 6/16/2017.
+ */
+
+public class NetworkUtils {
+
+    private static final String NEWS_URL=
+            "https://newsapi.org/v1/articles?source=the-next-web&sortBy=latest&apiKey=7369b6d11aad4d7899f70be9cae53ff8=";
+
+
+}
