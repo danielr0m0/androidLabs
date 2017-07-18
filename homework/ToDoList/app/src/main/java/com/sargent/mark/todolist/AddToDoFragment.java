8c022a6b88b66686695ce51b1455a74870ecda17
DanielrR0m0 <drdanielromo@gmail.com>
2017-07-18 16:19:39 -0700

    working version of the views

--- homework/ToDoList/app/src/main/java/com/sargent/mark/todolist/AddToDoFragment.java
+++ homework/ToDoList/app/src/main/java/com/sargent/mark/todolist/AddToDoFragment.java
@@ -6,10 +6,14 @@ import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TimePicker;
+
+import com.sargent.mark.todolist.data.Category;
 
 import java.util.Calendar;
 
@@ -21,6 +25,7 @@ public class AddToDoFragment extends DialogFragment{
 
     private EditText toDo;
     private DatePicker dp;
+    private Spinner spinner;
     private Button add;
     private final String TAG = "addtodofragment";
 
@@ -38,12 +43,16 @@ public class AddToDoFragment extends DialogFragment{
         toDo = (EditText) view.findViewById(R.id.toDo);
         dp = (DatePicker) view.findViewById(R.id.datePicker);
         add = (Button) view.findViewById(R.id.add);
+        spinner = (Spinner) view.findViewById(R.id.spinner);
 
         final Calendar c = Calendar.getInstance();
         int year = c.get(Calendar.YEAR);
         int month = c.get(Calendar.MONTH);
         int day = c.get(Calendar.DAY_OF_MONTH);
         dp.updateDate(year, month, day);
+
+        spinner = (Spinner) view.findViewById(R.id.category);
+        spinner.setAdapter(new ArrayAdapter<Category>(this.getActivity(), R.layout.support_simple_spinner_dropdown_item, Category.values()));
 
 
         add.setOnClickListener(new View.OnClickListener() {
